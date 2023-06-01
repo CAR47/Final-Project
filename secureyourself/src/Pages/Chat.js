ec71ad1acfc5bf723533100d904ae1a23a755978
Ashfaq Ramzan <92735391+CAR47@users.noreply.github.com>
2023-06-01 14:32:15 +0500

    Chat Page

--- secureyourself/src/Pages/Chat.js
+++ secureyourself/src/Pages/Chat.js
@@ -1,60 +1,55 @@
-import React, { Component } from 'react';
-import { Button, Form, Row, Col, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-class Chat extends Component {
-  state = {
-    messages: [],
-    message: '',
-  };
+function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
 
-  handleSend = () => {
-    const { message } = this.state;
-
-    if (message.trim() === '') {
-      return;
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, newMessage]);
+      setNewMessage('');
     }
-
-    // Send the message to the server.
-
-    // Update the state with the new message.
-    this.setState({
-      messages: [...this.state.messages, message],
-      message: '',
-    });
   };
 
-  render() {
-    return (
-      <Container className="d-flex justify-content-center">
-        <h1>Community Chat</h1>
-        <Container className="message-container">
-          <ul>
-            {this.state.messages.map((message) => (
-              <li key={message}>{message}</li>
-            ))}
-          </ul>
-        </Container>
-        <Container className="input-container">
-          <Form onSubmit={this.handleSend}>
-            <Row>
-              <Col>
-                <input
-                  type="text"
-                  name="message"
-                  placeholder="Enter your message"
-                  value={this.state.message}
-                  onChange={(e) => this.setState({ message: e.target.value })}
-                />
-              </Col>
-              <Col>
-                <Button type="submit">Send</Button>
-              </Col>
-            </Row>
-          </Form>
-        </Container>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Chat Page</h1>
+        </Col>
+      </Row>
+      <Container>
+        <Row>
+          <Col>
+            <div className="message-container">
+              {messages.map((message, index) => (
+                <div key={index}>{message}</div>
+              ))}
+            </div>
+          </Col>
+        </Row>
       </Container>
-    );
-  }
+      <Row>
+        <Col>
+          <Form onSubmit={handleMessageSubmit}>
+            <Form.Group controlId="newMessage">
+              <Form.Control
+                type="text"
+                placeholder="Type your message..."
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
+            </Form.Group><br />
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Chat;
